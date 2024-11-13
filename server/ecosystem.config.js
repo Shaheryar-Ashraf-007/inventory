7800db597f25a5acd46d3c93f50331a25b1d82d6
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2024-11-13 22:59:44 +0500

    minor changes

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
     apps: [
       {
         name: "inventory-management",
@@ -11,3 +11,5 @@ module.exports = {
       },
     ],
   };
+  
+  module.exports = config;
