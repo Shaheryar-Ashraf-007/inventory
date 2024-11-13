cfb54a9d9c8403fe59203f49cd7cc263d9ab5ec4
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2024-11-13 23:26:19 +0500

    minor changes

--- server/ecosystem.config.mjs
+++ server/ecosystem.config.mjs
@@ -2,7 +2,7 @@ export default {
     apps: [
       {
         name: "inventory-management-system-server",
-        script: "nodemon",
+        script: "npm",
         args: "src/index.js",
         env: {
           NODE_ENV: "development",
