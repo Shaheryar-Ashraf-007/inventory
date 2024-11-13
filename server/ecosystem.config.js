41ac15fac3dc00770bb04d3a48ecdb445b75acb1
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2024-11-13 23:41:42 +0500

    minor changes

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,15 +1,13 @@
 export default {
-    apps: [
-      {
-        name: "inventory-management-system-server",
-        script: "src/index.js",
-        interpreter: "nodemon", // Use nodemon if desired
-
-
-        env: {
-          NODE_ENV: "development",
-          ENV_VAR1: "environment-variable",
-        },
+  apps: [
+    {
+      name: "inventory-management-system-server",
+      script: "server/src/index.js", // Path to your entry file
+      interpreter: "node",
+      env: {
+        NODE_ENV: "development",
+        ENV_VAR1: "environment-variable",
       },
-    ],
-  };
+    },
+  ],
+};
