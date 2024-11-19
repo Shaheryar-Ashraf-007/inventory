1f816133421e3c9419ee1dad5c3e00d215136ab8
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2024-11-20 01:14:34 +0500

    minor commit

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,14 +1,13 @@
-export default {
+module.exports = {
   apps: [{
     name: "inventory-management-system-server",
     script: "/root/inventory/server/src/index.js",
     watch: true,
     env: {
-      "NODE_ENV": "development",
+      NODE_ENV: "development",
     },
     env_production: {
-      "NODE_ENV": "production"
+      NODE_ENV: "production"
     }
   }]
-}
-
+};
